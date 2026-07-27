f6f3828917260affebbcc88b08b34618f35fe59a
swadhin1530 <swadhinpatro15@gmail.com>
2026-07-27 23:17:13 +0530

    Add files via upload

---
+++ addnumbers.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* current = &dummy;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int val1 = (l1 != nullptr) ? l1->val : 0;
+            int val2 = (l2 != nullptr) ? l2->val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            int digit = sum % 10;
+
+            current->next = new ListNode(digit);
+            current = current->next;
+
+            if (l1 != nullptr)
+                l1 = l1->next;
+
+            if (l2 != nullptr)
+                l2 = l2->next;
+        }
+
+        return dummy.next;
+    }
+};
+
+// Function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val;
+        if (head->next != nullptr)
+            cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Number 342
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    // Number 465
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    Solution obj;
+    ListNode* result = obj.addTwoNumbers(l1, l2);
+
+    cout << "Result: ";
+    printList(result);
+
+    return 0;
+}
