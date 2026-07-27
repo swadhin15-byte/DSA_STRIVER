0b8cee9d3796a9c0daeb8d1360a75c71e0faf40d
swadhin1530 <swadhinpatro15@gmail.com>
2026-07-27 23:00:56 +0530

    Add files via upload

---
+++ SUM.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> ans = obj.twoSum(nums, target);
+
+    cout << "Indices: ";
+    for (int i : ans) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
