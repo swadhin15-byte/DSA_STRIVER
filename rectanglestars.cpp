23d52b3ba49e80dfeea5b8103596c3e4ecf023be
swadhin1530 <swadhinpatro15@gmail.com>
2026-09-03 23:23:18 +0530

    Add files via upload

---
+++ rectanglestars.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"enter number of rows:"<<endl;
+    cin>>x;
+    cout<<"enter number of columns:"<<endl;
+    cin>>y;
+    for( int i=1;i<=x;i++){
+        for(int j=1;j<=y;j++){
+            cout<<"* ";
+            
+        }
+        
+        cout<<endl;
+    }
+    
+    
+}
