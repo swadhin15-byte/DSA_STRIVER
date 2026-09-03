841c84163a6468f49ea9a8d14b482f6bf7f647ed
swadhin1530 <swadhinpatro15@gmail.com>
2026-09-03 23:22:37 +0530

    Add files via upload

--- triangle.cpp
+++ triangle.cpp
@@ -1,18 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a1,a2,a3;
-    cout<<"enter the angle 1 of triangle:";
-    cin>>a1;
-    cout<<"enter the angle 2 of triangle:";
-    cin>>a2;
-    cout<<"enter the angle 3 of triangle:";
-    cin>>a3;
-    if(a1+a2+a3==180){
-        cout<<"it is triangle.";
-
-    }
-    else{
-        cout<<"it is not a triangle.";
+    int x;
+    cout<<"enter no of columns:";
+    cin>>x;
+    for(int i=1;i<=x;i++){
+        for(int j=1;j<=i;j++){
+            cout<<" *";
+        }
+        cout<<endl;
     }
 }
