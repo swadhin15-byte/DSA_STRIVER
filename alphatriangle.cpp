b7559bc56a907ed655203da60f87d7e708db4a8b
swadhin1530 <swadhinpatro15@gmail.com>
2026-09-05 23:06:52 +0530

    Add files via upload

---
+++ alphatriangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter the number of columns";
+    cin>>x;
+    for(int i=1;i<=x;i++){
+        for(int j=1;j<=i;j++){
+            cout<<(char)(i+96);
+        }
+        cout<<endl;
+    }
+}
